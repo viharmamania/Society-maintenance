cfea24db06a6d945bdfb623ab0ac9ad667697c58
Vihar Mamania <Vihar1903@gmail.com>
2015-11-21 14:10:38 +0530

    Bill generation Code

--- Society/src/com/vhi/hsm/controller/manager/BillManager.java
+++ Society/src/com/vhi/hsm/controller/manager/BillManager.java
@@ -23,43 +23,43 @@ import com.vhi.hsm.utils.Constants;
  *
  */
 public class BillManager {
-	
-	private static String readProperties = "select * from "+ Constants.Table.Property.TABLE_NAME +
-			" where " + Constants.Table.Society.FieldName.SOCIETY_ID + "=?";
-	
+
+	private static String readProperties = "select * from " + Constants.Table.Property.TABLE_NAME + " where "
+			+ Constants.Table.Society.FieldName.SOCIETY_ID + "=?";
+
 	/**
 	 * Generates all the bills for society
 	 * 
 	 * @param societyId
 	 *            the id of society for which bills need to be generated
-	 * @return 
+	 * @return
 	 */
-	public static List<Bill> generateBill(int societyId) {
+	public static synchronized List<Bill> generateBill(int societyId) {
 		List<Bill> societyBills = new ArrayList<>();
 		Set<Property> properties = new HashSet<>();
 
 		try {
 			PreparedStatement readStatement = SQLiteManager.getPreparedStatement(readProperties);
 			readStatement.setInt(1, societyId);
-			
+
 			ResultSet result = readStatement.getResultSet();
-			
-			//adding the properties result in Property Hashmap
+
+			// adding the properties result in Property Hashmap
 			Property.addProperties(result);
-			
-			//fetching the List of properties for this societyId
+
+			// fetching the List of properties for this societyId
 			do {
 				result.next();
 				properties.add(Property.read(result.getInt(Constants.Table.Property.FieldName.PROPERTY_ID)));
 			} while (!result.isAfterLast());
-			
-			//generating individual bills and adding to list of bills
+
+			// generating individual bills and adding to list of bills
 			for (Property property : properties) {
 				societyBills.add(generatePropertySpecificBill(property));
 			}
 
-			
-			
+			// updating properties net_payable amount
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -78,16 +78,16 @@ public class BillManager {
 		double billAmount = 0.0;
 		Bill bill = Bill.create();
 		ArrayList<Integer> chargeIds = new ArrayList<>();
-		
-		//get All charges for this particular property
+
+		// get All charges for this particular property
 		chargeIds.addAll(getChargeIds(property));
-		
-		//calculate actual amount by adding charges for all chargeIds
-		for(Integer chargeId: chargeIds){
+
+		// calculate actual amount by adding charges for all chargeIds
+		for (Integer chargeId : chargeIds) {
 			Charge read = Charge.read(property.getSocietyId(), chargeId);
 			billAmount += read.getAmount();
 		}
-		
+
 		long currentTime = System.currentTimeMillis();
 		bill.setPropertyId(property.getPropertyId());
 		bill.setSocietyId(property.getSocietyId());
@@ -96,7 +96,14 @@ public class BillManager {
 		bill.setCancelled(false);
 		bill.setAssignedCharges(chargeIds);
 		bill.setAmount(billAmount);
-		
+
+		// saving bill in DB
+		Bill.save(bill, false);
+
+		// updating this properties net payable
+		property.setNetPayable(property.getNetPayable() + billAmount);
+		Property.save(property, false);
+
 		return bill;
 	}
 
@@ -107,47 +114,43 @@ public class BillManager {
 	 * @return
 	 */
 	private static Collection<? extends Integer> getChargeIds(Property property) {
-		
+
 		Set<Integer> chargeIds = new HashSet<>();
-		StringBuilder query= new StringBuilder();
-		
-		//fetching charges for this properties group type
-		query.append("select "+ Constants.Table.Charge.FieldName.CHARGE_ID 
-				+" from "+ Constants.Table.ChargeToPropertyGroup.TABLE_NAME
-				+ " where " + Constants.Table.PropertyGroup.FieldName.PROPERTY_GROUP 
-				+ " = ( select "+ Constants.Table.PropertyGroup.FieldName.PROPERTY_GROUP 
-				+ " from " + Constants.Table.FloorPlanDesign.TABLE_NAME
-				+ " where " + Constants.Table.Society.FieldName.SOCIETY_ID + " =? " 
-				+ " and " + Constants.Table.FloorPlanDesign.FieldName.PROPERTY_NUMBER + " =? )");
-		
+		StringBuilder query = new StringBuilder();
+
+		// fetching charges for this properties group type
+		query.append("select " + Constants.Table.Charge.FieldName.CHARGE_ID + " from "
+				+ Constants.Table.ChargeToPropertyGroup.TABLE_NAME + " where "
+				+ Constants.Table.PropertyGroup.FieldName.PROPERTY_GROUP + " = ( select "
+				+ Constants.Table.PropertyGroup.FieldName.PROPERTY_GROUP + " from "
+				+ Constants.Table.FloorPlanDesign.TABLE_NAME + " where " + Constants.Table.Society.FieldName.SOCIETY_ID
+				+ " =? " + " and " + Constants.Table.FloorPlanDesign.FieldName.PROPERTY_NUMBER + " =? )");
+
 		query.append(" union ");
-		
-		//fetching charges for this property type 
-		query.append("select "+ Constants.Table.Charge.FieldName.CHARGE_ID 
-				+" from "+ Constants.Table.ChargeToPropertyType.TABLE_NAME
-				+ " where " + Constants.Table.PropertyType.FieldName.PROPERTY_TYPE 
-				+ " = ( select "+ Constants.Table.PropertyType.FieldName.PROPERTY_TYPE 
-				+ " from " + Constants.Table.FloorPlanDesign.TABLE_NAME
-				+ " where " + Constants.Table.Society.FieldName.SOCIETY_ID + " =? " 
-				+ " and " + Constants.Table.FloorPlanDesign.FieldName.PROPERTY_NUMBER + " =? )");
-		
+
+		// fetching charges for this property type
+		query.append("select " + Constants.Table.Charge.FieldName.CHARGE_ID + " from "
+				+ Constants.Table.ChargeToPropertyType.TABLE_NAME + " where "
+				+ Constants.Table.PropertyType.FieldName.PROPERTY_TYPE + " = ( select "
+				+ Constants.Table.PropertyType.FieldName.PROPERTY_TYPE + " from "
+				+ Constants.Table.FloorPlanDesign.TABLE_NAME + " where " + Constants.Table.Society.FieldName.SOCIETY_ID
+				+ " =? " + " and " + Constants.Table.FloorPlanDesign.FieldName.PROPERTY_NUMBER + " =? )");
+
 		query.append(" union ");
-		
-		//fetching charges for this specific property 
-		query.append("select "+ Constants.Table.Charge.FieldName.CHARGE_ID 
-				+" from "+ Constants.Table.ChargeToProperty.TABLE_NAME
-				+ " where " + Constants.Table.Property.FieldName.PROPERTY_ID + " =? ");
-		
-		
+
+		// fetching charges for this specific property
+		query.append("select " + Constants.Table.Charge.FieldName.CHARGE_ID + " from "
+				+ Constants.Table.ChargeToProperty.TABLE_NAME + " where "
+				+ Constants.Table.Property.FieldName.PROPERTY_ID + " =? ");
+
 		query.append(" union ");
-		
-		//fetching charges which are applicable by-default to every property
-		query.append("select " + Constants.Table.Charge.FieldName.CHARGE_ID 
-				+ " from " + Constants.Table.Charge.TABLE_NAME 
-				+ " where " + Constants.Table.Charge.FieldName.IS_DEFAULT + "=1");
+
+		// fetching charges which are applicable by-default to every property
+		query.append("select " + Constants.Table.Charge.FieldName.CHARGE_ID + " from "
+				+ Constants.Table.Charge.TABLE_NAME + " where " + Constants.Table.Charge.FieldName.IS_DEFAULT + "=1");
 
 		PreparedStatement fetchChargesStmt = SQLiteManager.getPreparedStatement(query.toString());
-		
+
 		try {
 			fetchChargesStmt.setInt(1, property.getPropertyNumber());
 			fetchChargesStmt.setInt(2, property.getPropertyNumber());
@@ -159,7 +162,7 @@ public class BillManager {
 					chargeIds.add(result.getInt(Constants.Table.Charge.FieldName.CHARGE_ID));
 				} while (!result.isAfterLast());
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
