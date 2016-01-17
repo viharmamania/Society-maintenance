955fb77e12b67b86ed1deb5ab51f4c4fa02b84c3
Vihar Mamania <Vihar1903@gmail.com>
2016-01-17 15:46:59 +0530

    Generating Payment receipts code

--- Society/src/com/vhi/hsm/controller/manager/PDFManager.java
+++ Society/src/com/vhi/hsm/controller/manager/PDFManager.java
@@ -2,19 +2,35 @@ package com.vhi.hsm.controller.manager;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.vhi.hsm.db.SQLiteManager;
 import com.vhi.hsm.model.Bill;
 import com.vhi.hsm.model.Charge;
+import com.vhi.hsm.model.ModeOfPayment;
+import com.vhi.hsm.model.Payment;
 import com.vhi.hsm.model.Property;
+import com.vhi.hsm.utils.Constants;
 
 public class PDFManager {
+
+	static String paymentBillContent = "Received with thanks from Shri/Smt./M /s. [1] for Flat/Shop/Garage No. [2] , \nthe sum of Rupees [3] by [4] [5], in part/full payment.";
 
 	public static void generateBillPDF(List<Bill> bills) throws DocumentException, FileNotFoundException {
 		Document document = new Document();
@@ -22,22 +38,30 @@ public class PDFManager {
 				new FileOutputStream("Maintenance Bills " + Calendar.getInstance().get(Calendar.YEAR) + " "
 						+ Calendar.getInstance().get(Calendar.MONTH) + ".pdf"));
 		document.open();
-		
+
+		Image instance;
+		try {
+			instance = Image.getInstance("C:/Users/Vihar/Desktop/logo.png");
+			document.add(instance);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		PdfPTable headerTable = new PdfPTable(1);
 		headerTable.addCell(SystemManager.society.getName());
 		headerTable.addCell(SystemManager.society.getAddress());
-		headerTable.addCell(SystemManager.society.getRegistrationNumber() + " " + SystemManager.society.getRegistrationDate());
-		
+		headerTable.addCell(
+				SystemManager.society.getRegistrationNumber() + " " + SystemManager.society.getRegistrationDate());
+
 		Bill bill;
 		ArrayList<Integer> billAssignedCharges;
 		for (int i = 0; i < bills.size(); i++) {
 			bill = null;
 			bill = bills.get(i);
 			if (bill != null) {
-				//Society Information		
+				// Society Information
 				document.add(headerTable);
-				
-				//Owner Information
+
+				// Owner Information
 				PdfPTable flatTable = new PdfPTable(2);
 				Property property = Property.read(bill.getPropertyId());
 				if (property != null) {
@@ -47,14 +71,14 @@ public class PDFManager {
 					flatTable.addCell(Double.toString(bill.getAmount()));
 					document.add(flatTable);
 				}
-				
-				//Bill charges
+
+				// Bill charges
 				PdfPTable billTable = new PdfPTable(2);
 				billTable.addCell("Charge");
 				billTable.addCell("Amount");
 				billAssignedCharges = null;
 				billAssignedCharges = bill.getAssignedCharges();
-				for(Integer chargeId : billAssignedCharges) {
+				for (Integer chargeId : billAssignedCharges) {
 					Charge charge = Charge.read(SystemManager.society.getSocietyId(), chargeId.intValue());
 					if (charge != null) {
 						billTable.addCell(charge.getDescription());
@@ -71,5 +95,86 @@ public class PDFManager {
 
 		document.close();
 		pdfWriter.close();
+	}
+
+	public static void generateReceiptsPDF(List<Payment> payments)
+			throws DocumentException, MalformedURLException, IOException {
+
+		Document document = new Document();
+		PdfWriter pdfWriter = PdfWriter.getInstance(document,
+				new FileOutputStream("Maintenance Bills Receipt" + Calendar.getInstance().get(Calendar.YEAR) + " "
+						+ Calendar.getInstance().get(Calendar.MONTH) + ".pdf"));
+		document.open();
+
+		PdfPTable headerTable = new PdfPTable(1);
+		headerTable.addCell(SystemManager.society.getName());
+		headerTable.addCell(SystemManager.society.getAddress());
+		headerTable.addCell(
+				SystemManager.society.getRegistrationNumber() + " " + SystemManager.society.getRegistrationDate());
+
+		Payment payment;
+		for (int i = 0; i < payments.size(); i++) {
+			payment = null;
+			payment = payments.get(i);
+			if (payment != null) {
+				PdfPTable flatTable = new PdfPTable(2);
+
+				PdfPCell receiptNoCell = new PdfPCell(new Phrase("Receipt No:" + payment.getPaymentId()));
+				receiptNoCell.setBorder(Rectangle.NO_BORDER);
+//				receiptNoCell.setBorderColor(new Color(255, 255, 45));
+				PdfPCell dateCell = new PdfPCell(new Phrase("Date:" + new Date()));
+				dateCell.setBorder(Rectangle.NO_BORDER);
+//				dateCell.setBorderColor(new Color(255, 255, 45));
+				flatTable.addCell(dateCell);
+
+				// flatTable.addCell("Receipt No:" + payment.getPaymentId());
+				// flatTable.addCell("Date:" + new Date());
+				document.add(flatTable);
+			}
+			Property property = Property.read(payment.getPaymentId());
+			paymentBillContent = paymentBillContent.replace("[1]", property.getOwnerName());
+			paymentBillContent = paymentBillContent.replace("[2]", property.getPropertyName());
+			paymentBillContent = paymentBillContent.replace("[3]", String.valueOf(payment.getAmount()));
+			paymentBillContent = paymentBillContent.replace("[4]", payment.getModeOfPayment());
+			if (payment.getModeOfPayment().equalsIgnoreCase(ModeOfPayment.CHEQUE.name())) {
+				paymentBillContent = paymentBillContent.replace("[5]", " by Cheque No: " + payment.getChequeNumber());
+			} else if (payment.getModeOfPayment().equalsIgnoreCase(ModeOfPayment.CASH.name()))
+				paymentBillContent = paymentBillContent.replace("[5]", "");
+
+			Integer billId = null;
+			String query = "select bill_id from bill where payment_id =" + payment.getPaymentId()
+					+ " order by bill_id  ASC";
+			try {
+				ResultSet executeQuery = SQLiteManager.executeQuery(query);
+				if (executeQuery != null & executeQuery.next()) {
+					do {
+						billId = executeQuery.getInt(Constants.Table.Payment.FieldName.PAYMENT_ID);
+						executeQuery.next();
+					} while (!executeQuery.isAfterLast());
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if (billId != null)
+				paymentBillContent = paymentBillContent.replace("[6]", " on account of Bill No. " + billId);
+
+			document.add(new Paragraph(paymentBillContent));
+			document.add(new Paragraph("\n"));
+			document.add(new Paragraph("\n"));
+			Image instance = Image.getInstance("C:/Users/Vihar/Desktop/Y.jpg");
+			document.add(instance);
+
+			document.add(new Paragraph("Rs: " + payment.getAmount()));
+
+			if (i != (payments.size() - 1)) {
+				document.newPage();
+			}
+		}
+
+		document.close();
+		pdfWriter.close();
+
 	}
 }
--- Society/src/com/vhi/hsm/view/panel/DashBoard.java
+++ Society/src/com/vhi/hsm/view/panel/DashBoard.java
@@ -6,6 +6,12 @@ import java.awt.GridLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.swing.BorderFactory;
@@ -24,12 +30,16 @@ import org.apache.log4j.Logger;
 import com.itextpdf.text.DocumentException;
 import com.vhi.hsm.controller.manager.BillManager;
 import com.vhi.hsm.controller.manager.PDFManager;
+import com.vhi.hsm.controller.manager.PreviewManager;
 import com.vhi.hsm.controller.manager.SystemManager;
+import com.vhi.hsm.db.SQLiteManager;
 import com.vhi.hsm.model.Bill;
+import com.vhi.hsm.utils.Constants;
 import com.vhi.hsm.view.AssetTypeScreen;
 import com.vhi.hsm.view.ChargeScreen;
 import com.vhi.hsm.view.Payment;
 import com.vhi.hsm.view.PropertyView;
+import com.vhi.hsm.view.TempChargesConfirmationView;
 
 public class DashBoard extends JFrame implements WindowListener {
 
@@ -45,7 +55,7 @@ public class DashBoard extends JFrame implements WindowListener {
 
 	private JButton //propertyPayButton, 
 					propertyAssetButton, propertyViewButton, billGenerateButton, billViewButton,
-			chargeViewButton, paymentViewButton, makePaymentButton;
+			chargeViewButton, paymentViewButton, makePaymentButton,paymentReceiptsButton;
 
 	public DashBoard() {
 
@@ -68,6 +78,16 @@ public class DashBoard extends JFrame implements WindowListener {
 			new AssetTypeScreen(this);
 		});
 
+		paymentReceiptsButton = new JButton("Generate Payment Receipts");
+		paymentReceiptsButton.addActionListener(e -> {
+			try {
+				generateMonthlyPaymentReceipts();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+
+		});
+		
 		propertyViewButton = new JButton("View & Edit Properties");
 		propertyViewButton.addActionListener(e -> {
 			new PropertyView(this);
@@ -108,11 +128,24 @@ public class DashBoard extends JFrame implements WindowListener {
 		setVisible(true);
 	}
 
+	private List<Bill> generateBillPreview() {
+		List<Bill> bills = null;
+		try {
+			bills = PreviewManager.generateBill(SystemManager.society.getSocietyId());
+			//PDFManager.generateBillPDF(bills);
+//			JOptionPane.showMessageDialog(this, "The Bills have been generated successfully ", "Success", JOptionPane.INFORMATION_MESSAGE);
+		} catch (Exception e) {
+			LOG.error(e);
+		}
+		return bills;
+	}
+	
 	private void generateBill() {
 		try {
 			List<Bill> bills = BillManager.generateBill(SystemManager.society.getSocietyId());
 			PDFManager.generateBillPDF(bills);
 			JOptionPane.showMessageDialog(this, "The Bills have been generated successfully ", "Success", JOptionPane.INFORMATION_MESSAGE);
+			dispose();
 		} catch (FileNotFoundException | DocumentException e) {
 			LOG.error(e.getMessage());
 		}
@@ -142,9 +175,10 @@ public class DashBoard extends JFrame implements WindowListener {
 		c.gridwidth = 1;
 		getContentPane().add(infoPanel, c);
 
-		paymentPanel.setLayout(new GridLayout(0, 1));
+		paymentPanel.setLayout(new GridLayout(0, 2));
 		paymentPanel.add(paymentViewButton);
 		paymentPanel.add(makePaymentButton);
+		paymentPanel.add(paymentReceiptsButton);
 
 		billPanel.setLayout(new GridLayout(0, 1));
 		billPanel.add(billViewButton);
@@ -228,6 +262,43 @@ public class DashBoard extends JFrame implements WindowListener {
 	@Override
 	public void windowOpened(WindowEvent arg0) {
 
+	}
+	
+	private void generateMonthlyPaymentReceipts() throws SQLException, DocumentException, MalformedURLException, IOException {
+		Calendar instance = Calendar.getInstance();
+//		instance.add(Calendar.MONTH, -1);
+		instance.set(Calendar.DAY_OF_MONTH, 1);
+		instance.set(Calendar.HOUR_OF_DAY, 0);
+		instance.set(Calendar.MINUTE, 0);
+		instance.set(Calendar.SECOND, 0);
+		instance.set(Calendar.MILLISECOND, 0);
+
+		long startTime = instance.getTime().getTime();
+
+		instance.set(Calendar.DAY_OF_MONTH, instance.getActualMaximum(Calendar.DAY_OF_MONTH));
+		instance.set(Calendar.HOUR_OF_DAY, instance.getActualMaximum(Calendar.HOUR_OF_DAY));
+		instance.set(Calendar.MINUTE, instance.getActualMaximum(Calendar.MINUTE));
+		instance.set(Calendar.SECOND, instance.getActualMaximum(Calendar.SECOND));
+		instance.set(Calendar.MILLISECOND, instance.getActualMaximum(Calendar.MILLISECOND));
+		long endTime = instance.getTime().getTime();
+
+		String query = "select payment_id from payment where payment_date >= '" + startTime + "' and payment_date <='"
+				+ endTime + "' and is_cancelled = 0";
+		ResultSet executeQuery = SQLiteManager.executeQuery(query);
+		List<Integer> paymentIds = new ArrayList<>();
+		if (executeQuery != null & executeQuery.next()) {
+			do {
+				paymentIds.add(executeQuery.getInt(Constants.Table.Payment.FieldName.PAYMENT_ID));
+				executeQuery.next();
+			} while (!executeQuery.isAfterLast());
+		}
+
+		List<com.vhi.hsm.model.Payment> payments = new ArrayList<>();
+		for (Integer i : paymentIds) {
+			payments.add(com.vhi.hsm.model.Payment.read(i));
+		}
+
+		PDFManager.generateReceiptsPDF(payments);
 	}
 
 }
