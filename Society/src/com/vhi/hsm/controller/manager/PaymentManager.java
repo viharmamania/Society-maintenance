99174c1d0741f6def9cff502e403e12212fc4ba0
Hardik Senghani <hardik.patel0491@gmail.com>
2016-02-21 15:33:33 +0530

    Logic for updating property net-payable while makeing payment

--- Society/src/com/vhi/hsm/controller/manager/PaymentManager.java
+++ Society/src/com/vhi/hsm/controller/manager/PaymentManager.java
@@ -3,16 +3,16 @@ package com.vhi.hsm.controller.manager;
 import java.util.ArrayList;
 
 import com.vhi.hsm.model.Bill;
-import com.vhi.hsm.model.BillCharge;
 import com.vhi.hsm.model.Payment;
 import com.vhi.hsm.model.Property;
 
 public class PaymentManager {
 
+	@SuppressWarnings("deprecation")
 	public static synchronized ArrayList<Bill> makePayment(Payment payment) {
 		
-		double remainingAmount;
 		ArrayList<Bill> paidBills = new ArrayList<Bill>();
+		double newNetPaybale;
 
 //		boolean commit = true;
 //
@@ -24,35 +24,46 @@ public class PaymentManager {
 
 				if (property != null) {
 					
-					ArrayList<Bill> unpaidBills = BillManager.getUnpaidBills(property);
-					
-					double fineAmount;
-					BillCharge billCharge;
-					
-					remainingAmount = property.getNetPayable() + payment.getAmount();
-					
-					for (Bill bill : unpaidBills) {
-						fineAmount = BillManager.getLateFineAmount(bill, payment.getPaymentDate());
-						bill.setAmount(bill.getAmount() + fineAmount);
-						if ( remainingAmount >= bill.getAmount() ) {
-							remainingAmount -= bill.getAmount();
-							billCharge = BillCharge.create(bill.getBillId(), -1); //-1 is for late fine charge id
-							billCharge.setAmount(fineAmount);
-							BillCharge.save(billCharge, true);
-							bill.setPaymentId(payment.getPaymentId());
-							Bill.save(bill, false);
-							paidBills.add(bill);
-						} else {
-							bill.setAmount(bill.getAmount() - fineAmount);
-							break;
-						}
-					}
+//					ArrayList<Bill> unpaidBills = BillManager.getUnpaidBills(property);
+//					
+//					double fineAmount;
+//					BillCharge billCharge;
+//					
+//					remainingAmount = property.getNetPayable() + payment.getAmount();
+//					
+//					for (Bill bill : unpaidBills) {
+//						fineAmount = BillManager.getLateFineAmount(bill, payment.getPaymentDate());
+//						bill.setAmount(bill.getAmount() + fineAmount);
+//						if ( remainingAmount >= bill.getAmount() ) {
+//							remainingAmount -= bill.getAmount();
+//							billCharge = BillCharge.create(bill.getBillId(), -1); //-1 is for late fine charge id
+//							billCharge.setAmount(fineAmount);
+//							BillCharge.save(billCharge, true);
+//							bill.setPaymentId(payment.getPaymentId());
+//							Bill.save(bill, false);
+//							paidBills.add(bill);
+//						} else {
+//							bill.setAmount(bill.getAmount() - fineAmount);
+//							break;
+//						}
+//					}
 
 					// save the remaining balance to property
-					property.setNetPayable(remainingAmount);
-					if (!Property.save(property, false)) {
-//						commit = false;
+//					property.setNetPayable(remainingAmount);
+					
+					newNetPaybale = property.getNetPayable();
+					
+					/***
+					 * Add fine if Last paymentis done before due date and current payment date is after the due date
+					 */
+					if (payment.getPaymentDate().getDay() > SystemManager.society.getPaymentDueDate() && newNetPaybale > 0) {
+						Payment lastPayment = Payment.read(property.getLatestPaymentId());
+						if (lastPayment.getPaymentDate().getDay() >= SystemManager.society.getPaymentDueDate()) {
+							newNetPaybale = newNetPaybale * (1.0 + SystemManager.society.getLateFineInterest());
+						}
 					}
+					
+					property.setNetPayable(newNetPaybale);
 					
 					property.setLatestPaymentId(payment.getPaymentId());
 					Property.save(property, false);
