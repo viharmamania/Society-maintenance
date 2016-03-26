0df6fce30a1e00fe685879cd89c68e98b8ef4aa0
Vihar Mamania <Vihar1903@gmail.com>
2016-03-26 16:09:19 +0530

    Payment date check is added while making payment

--- Society/src/com/vhi/hsm/view/Payment.java
+++ Society/src/com/vhi/hsm/view/Payment.java
@@ -274,22 +274,22 @@ public class Payment extends JDialog implements WindowListener {
 						Double amount = null;
 						for (int j = 0; j < cellCount; j++) {
 							Cell cell = row.getCell(j);
-							if (j == 0 && cell!=null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
+							if (j == 0 && cell != null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
 								propertyName = cell.getStringCellValue();
 							}
-							if (j == 1 && cell!=null && cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+							if (j == 1 && cell != null && cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
 								amount = cell.getNumericCellValue();
 							}
-							if (j == 2 && cell!=null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
+							if (j == 2 && cell != null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
 								modeOfPayment = cell.getStringCellValue();
 							}
-							if (j == 3 && cell!=null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
+							if (j == 3 && cell != null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
 								chequeNo = cell.getStringCellValue();
 							}
-							if (j == 4 && cell!=null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
+							if (j == 4 && cell != null && cell.getCellType() == Cell.CELL_TYPE_STRING) {
 								remarks = cell.getStringCellValue();
 							}
-							if (j == 5 && cell!=null) {
+							if (j == 5 && cell != null) {
 								paymentDate = cell.getDateCellValue();
 							}
 						}
@@ -392,6 +392,20 @@ public class Payment extends JDialog implements WindowListener {
 		if (!paymentDateField.getText().trim().matches(
 				"^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$")) {
 			JOptionPane.showMessageDialog(this, "Please Enter Correct Date", "Error", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			Date paymentDate = sdf.parse(paymentDateField.getText());
+			Date today = new Date(System.currentTimeMillis());
+			if (today.before(paymentDate)) {
+				JOptionPane.showMessageDialog(this, "Payment date can not be after today's date", "Error", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+		} catch (ParseException e) {
+			LOG.error(e.getMessage());
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
 
